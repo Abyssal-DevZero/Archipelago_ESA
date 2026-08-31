aca64ba72a1d00a2ca41e5110f221cfe05407b5f
Lucius <abyssaldev@abyssalhorizon.org>
2026-08-31 16:59:35 +0200

    Wrote basic create_events
    Pillars not included yet

--- worlds/esa/locations.py
+++ worlds/esa/locations.py
@@ -114,42 +114,18 @@ def create_regular_locations(world: ESAWorld) -> None:
     )
     derelict_ship.add_locations(derelict_ship_locations, ESALocation)
 
-def create_events(world: APQuestWorld) -> None:
-    # Sometimes, the player may perform in-game actions that allow them to progress which are not related to Items.
-    # In our case, the player must press a button in the top left room to open the final boss door.
-    # AP has something for this purpose: "Event locations" and "Event items".
-    # An event location is no different than a regular location, except it has the address "None".
-    # It is treated during generation like any other location, but then it is discarded.
-    # This location cannot be "sent" and its item cannot be "received", but the item can be used in logic rules.
-    # Since we are creating more locations and adding them to regions, we need to grab those regions again first.
-    top_left_room = world.get_region("Top Left Room")
-    final_boss_room = world.get_region("Final Boss Room")
 
-    # One way to create an event is simply to use one of the normal methods of creating a location.
-    button_in_top_left_room = APQuestLocation(world.player, "Top Left Room Button", None, top_left_room)
-    top_left_room.locations.append(button_in_top_left_room)
-
-    # We then need to put an event item onto the location.
-    # An event item is an item whose code is "None" (same as the event location's address),
-    # and whose classification is "progression". Item creation will be discussed more in items.py.
-    # Note: Usually, items are created in world.create_items(), which for us happens in items.py.
-    # However, when the location of an item is known ahead of time (as is the case with an event location/item pair),
-    # it is common practice to create the item when creating the location.
-    # Since locations also have to be finalized after world.create_regions(), which runs before world.create_items(),
-    # we'll create both the event location and the event item in our locations.py code.
-    button_item = items.APQuestItem("Top Left Room Button Pressed", ItemClassification.progression, None, world.player)
-    button_in_top_left_room.place_locked_item(button_item)
-
-    # A way simpler way to do create an event location/item pair is by using the region.create_event helper.
-    # Luckily, we have another event we want to create: The Victory event.
-    # We will use this event to track whether the player can win the game.
-    # The Victory event is a completely optional abstraction - This will be discussed more in set_rules().
-    final_boss_room.add_event(
-        "Final Boss Defeated", "Victory", location_type=APQuestLocation, item_type=items.APQuestItem
+#Event for Endbosses AI Mainframe and Mywah when Postgame is included
+def create_events(world: ESAWorld) -> None:
+    ai_mainframe = world.get_region("The A.I. Mainframe")
+    ai_mainframe.add_event(
+        "A.I. Mainframe Boss Defeated", "A.I. Mainframe Boss Defeated",
+        location_type=ESALocation, item_type=items.ESAItem,
     )
 
-    # If you create all your regions and locations line-by-line like this,
-    # the length of your create_regions might get out of hand.
-    # Many worlds use more data-driven approaches using dataclasses or NamedTuples.
-    # However, it is worth understanding how the actual creation of regions and locations works,
-    # That way, we're not just mindlessly copy-pasting! :)
+    if world.options.postgame:
+        forlorn_planet = world.get_region("The Forlorn Planet")
+        forlorn_planet.add_event(
+            "Mwyah Defeated", "Mwyah Defeated",
+            location_type=ESALocation, item_type=items.ESAItem,
+        )
